4fb856c942b6d136fe8d8edc458c62f1de219aca
Arthur Norat <norat.arthur@gmail.com>
2025-05-06 17:26:55 -0300

    feat: implement feature coordinators (AddExpense, Summary, Settings)

---
+++ Budget/Coordinators/SummaryCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  SummaryCoordinator.swift
+//  Budget
+//
+//  Created by Arthur Norat on 06/05/25.
+//
+
+import UIKit
+
+final class SummaryCoordinator: Coordinator {
+	
+	// MARK: - Coordinator Properties
+	var childCoordinators: [Coordinator] = []
+	var navigationController: UINavigationController
+	
+	// MARK: - Computed Properties
+	var rootViewController: UIViewController {
+		return navigationController.viewControllers.first!
+	}
+	
+	// MARK: - Initialization
+	init() {
+		self.navigationController = UINavigationController()
+		configureNavigationController()
+	}
+	
+	// MARK: - Coordinator Lifecycle
+	func start() {
+		let viewController = SummaryViewController()
+		navigationController.setViewControllers([viewController], animated: false)
+	}
+	
+	// MARK: - Private Methods
+	private func configureNavigationController() {
+		navigationController.navigationBar.prefersLargeTitles = true
+		// Customizações específicas do navigation
+	}
+}
