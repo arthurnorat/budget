f2b58c19dd2b7c23205d7a4fb6196d61971c798f
Arthur Norat <norat.arthur@gmail.com>
2025-05-06 16:53:11 -0300

    feat: implement Coordinator patern foundation

--- Budget/Application/AppCoordinator.swift
+++ Budget/Application/AppCoordinator.swift
@@ -6,15 +6,58 @@
 //
 
 import UIKit
+import Foundation
 
-final class AppCoordinator {
-	let window: UIWindow
+protocol Coordinator {
+	var childCoordinators: [Coordinator] { get set }
+	var navigationController: UINavigationController { get set }
+	
+	func start()
+}
 
+final class AppCoordinator: Coordinator {
+	
+	
+	var childCoordinators: [Coordinator] = []
+	var navigationController: UINavigationController
+	private var window: UIWindow
+	
 	init(window: UIWindow) {
 		self.window = window
+		self.navigationController = UINavigationController()
+		
 	}
-
+	
 	func start() {
-		// Em breve, colocaremos a ViewController aqui
+		
+		// Cria coordenadores para cada fluxo
+		let addExpenseCoordinator = AddExpenseCoordinator()
+		let summaryCoordinator = SummaryCoordinator()
+		let settingsCoordinator = SettingsCoordinator()
+		
+		// Inicia cada coordenador
+		addExpenseCoordinator.start()
+		summaryCoordinator.start()
+		settingsCoordinator.start()
+		
+		// Configura a TabBarController
+		let tabBarController = AppTabBarController(
+			addExpenseCoordinator: addExpenseCoordinator,
+			summaryCoordinator: summaryCoordinator,
+			settingsCoordinator: settingsCoordinator
+		)
+		
+		// Manter referência aos coordenadores
+		childCoordinators = [
+			addExpenseCoordinator,
+			summaryCoordinator,
+			settingsCoordinator
+		]
+		
+		window.rootViewController = tabBarController
+		window.makeKeyAndVisible()		
 	}
+		
 }
+
+
