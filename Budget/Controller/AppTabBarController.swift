e4fd7ef2d8e4a3823cfe752abd88d8183fd0a595
Arthur Norat <norat.arthur@gmail.com>
2025-05-06 16:54:17 -0300

    refactor: adapt AppTabBarController for Coordinator pattern

--- Budget/Controller/AppTabBarController.swift
+++ Budget/Controller/AppTabBarController.swift
@@ -8,49 +8,57 @@
 import UIKit
 
 final class AppTabBarController: UITabBarController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupTabs()
-        customizeAppearance()
-    }
-    
-    private func setupTabs() {
-        // 1. Instanciar as ViewControllers
-		let addExpenseVC = UINavigationController(rootViewController: AddExpenseViewController())
-        let summaryVC = UINavigationController(rootViewController: SummaryViewController())
-        let settingsVC = UINavigationController(rootViewController: SettingsViewController())
+	
+    init(
+		addExpenseCoordinator: AddExpenseCoordinator,
+		summaryCoordinator: SummaryCoordinator,
+		settingsCoordinator: SettingsCoordinator
+	) {
+		super.init(nibName: nil, bundle: nil)
 		
-		if let addExpenseController = addExpenseVC.viewControllers.first as? AddExpenseViewController,
-		   let summaryController = summaryVC.viewControllers.first as? SummaryViewController {
-			addExpenseController.delegate = summaryController
+		// Configurar as view controllers dos controladores
+		viewControllers = [
+			addExpenseCoordinator.navigationController,
+			summaryCoordinator.navigationController,
+			settingsCoordinator.navigationController
+		]
+		
+		// Configurar a comunicação entre as abas
+		if let addExpenseVC = addExpenseCoordinator.rootViewController as? AddExpenseViewController,
+		   let summaryVC = summaryCoordinator.rootViewController as? SummaryViewController {
+			addExpenseVC.delegate = summaryVC
 		}
-        
-        // 2. Definir ícones e títulos
-		addExpenseVC.tabBarItem = UITabBarItem(
+		
+		customizeAppearance()
+		
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func customizeAppearance() {
+		tabBar.tintColor = .systemBlue // Cor do ícone selecionado
+		tabBar.unselectedItemTintColor = .systemGray // Cor dos ícones inativos
+		tabBar.backgroundColor = .systemBackground // Fundo da TabBar
+		
+		// Configura os itens da TabBar
+		viewControllers?[0].tabBarItem = UITabBarItem(
 			title: "Adicionar",
 			image: UIImage(systemName: "plus"),
 			selectedImage: UIImage(systemName: "plus.circle.fill")
 		)
 		
-		summaryVC.tabBarItem = UITabBarItem(
-            title: "Gastos",
-            image: UIImage(systemName: "list.bullet"),
-            selectedImage: UIImage(systemName: "list.bullet.fill")
-        )
-        
-        settingsVC.tabBarItem = UITabBarItem(
-            title: "Config",
-            image: UIImage(systemName: "gear"),
-            selectedImage: UIImage(systemName: "gear.fill")
-        )
-        
-        // 3. Adicionar às tabs
-        setViewControllers([addExpenseVC, summaryVC, settingsVC], animated: true)
-    }
-    
-    private func customizeAppearance() {
-        tabBar.tintColor = .systemBlue // Cor do ícone selecionado
-        tabBar.unselectedItemTintColor = .systemGray // Cor dos ícones inativos
-        tabBar.backgroundColor = .systemBackground // Fundo da TabBar
-    }
+		viewControllers?[1].tabBarItem = UITabBarItem(
+			title: "Gastos",
+			image: UIImage(systemName: "list.bullet"),
+			selectedImage: UIImage(systemName: "list.bullet.fill")
+		)
+		
+		viewControllers?[2].tabBarItem = UITabBarItem(
+			title: "Config",
+			image: UIImage(systemName: "gear"),
+			selectedImage: UIImage(systemName: "gear.fill")
+		)
+	}
 }
